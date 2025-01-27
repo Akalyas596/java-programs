c1c2059016a45c89e853609711d86fd8d0f80d7c
akalya s <137234732+Akalyas596@users.noreply.github.com>
2025-01-27 22:08:08 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Positive{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0){
+System.out.println("positive number");
+}
+else
+{
+System.out.println("negative number");
+}
+}}
